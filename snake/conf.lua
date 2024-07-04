af2cd91264d45e762f8b421671d6c002d6b6f3cf
k4gnes <kardosagnes@gmail.com>
2024-07-04 23:11:31 +0200

    code refactor

--- snake/conf.lua
+++ snake/conf.lua
@@ -1,7 +1,13 @@
+--size for draw
+unit = 10
+--game area dimensions
+width = 128
+height = 75  
+
 function love.conf(t)
   --set window width and height from the specification
   t.window.width = 1280
   t.window.height = 750
   t.window.title = "snake"
   t.window.console = true  
- end
+end
--- snake/main.lua
+++ snake/main.lua
@@ -1,43 +1,163 @@
-require "game"
+--game states
+local states = { running = 'running', gameover = 'game over' }
+--tables for the snake and food
+local head = {}
+local tail = {}
+local apple = {}
+--directions 
+local left, right, up, down = false, false, false, false
+local dirX, dirY = 0,0
 
-GameStates ={ running= 'running', game_over='game over' }
-
+local speed = 0
+local timer = 0
 
 function love.load()
-  math.randomseed(os.time())
-  unit=10
-  width=128
-  height=75  
-  game_start()
+    -- initialize random number generator
+    math.randomseed(os.time())
+    --start/restart game
+    start()
 end
 
 function love.draw()
-  game_draw()
-  if state==GameStates.game_over then
-    --draw the game over screen
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.setNewFont(20)
-    love.graphics.printf("GAME OVER\nPress [SPACE] for a new game", 0, 750 / 2 - 50, 1280, "center")    
-  end  
+    --draw the snake's head rounded rectangle with size (width and height also equals unit)
+    love.graphics.setColor(0, 0.8, 0)
+    love.graphics.rectangle("fill", head[1] * unit, head[2] * unit, unit, unit, 5, 5)
+
+    --draw the tail rounded rectangle (like head)
+    love.graphics.setColor(0, 0.8, 0)
+    for _, v in ipairs(tail) do
+        love.graphics.rectangle("fill", v[1] * unit, v[2] * unit, unit, unit, 5, 5);
+    end
+
+    --draw the apple rounded rectangle just like head and tail
+    love.graphics.setColor(0.8, 0, 0)
+    love.graphics.rectangle("fill", apple[1] * unit, apple[2] * unit, unit, unit, 5, 5)
+
+    if state == states.gameover then
+        --draw the game over screen
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.setNewFont(20)
+        love.graphics.printf("GAME OVER\nPress [SPACE] for a new game", 0, height * 10 / 2 - 50, width * 10, "center")
+    end
 end
 
 function love.update(dt)
-  if state==GameStates.running then
-    game_update(dt)
-  end
+    if state == states.running then
+        --time limit for speed of snake
+        timer = timer + dt
+        if timer >= speed then
+            --set direction of the snake change its direction back only if has no tail
+            if up and (dirY == 0 or #tail == 0) then
+                dirX, dirY = 0, -1
+            elseif down and (dirY == 0 or #tail == 0) then
+                dirX, dirY = 0, 1
+            elseif left and (dirX == 0 or #tail == 0) then
+                dirX, dirY = -1, 0
+            elseif right and (dirX == 0 or #tail == 0) then
+                dirX, dirY = 1, 0
+            end
+
+            -- Movement
+            --moving the snake's head
+            head[1] = head[1] + dirX
+            head[2] = head[2] + dirY
+
+            --check food
+            if head[1] == apple[1] and head[2] == apple[2] then
+                --add food and increase the snake
+                apple = getfreepos()
+                table.insert(tail, { head[1] - dirX, head[2] - dirY })
+            end
+
+            --set snake tail parts
+            --from tail end to second
+            for i = #tail, 2, -1 do
+                tail[i] = tail[i - 1]
+            end
+            --first tail part behind the head
+            if #tail > 0 then
+                tail[1] = { head[1] - dirX, head[2] - dirY }
+            end
+
+            --check if game is over
+            --because snake's head is out of the screen
+            if head[1] < 0 or head[2] < 0 or head[1] > width - 1 or head[2] > height - 1 then
+                --game is over
+                state = states.gameover
+            end
+
+            --because snake's head is in the tail
+            for i, v in ipairs(tail) do
+                if head[1] == v[1] and head[2] == v[2] and i ~= 1 then
+                    --game is over
+                    state = states.gameover
+                end
+            end
+            timer = 0
+        end
+    end
 end
 
 function love.keypressed(key)
-  if key == "left" and state==GameStates.running then
-    left,right,up,down = true,false,false,false
-  elseif key == "right" and state==GameStates.running then
-    left,right,up,down = false, true, false, false
-  elseif key == "up" and state==GameStates.running then
+    if key == "left" and state == states.running then
+        left, right, up, down = true, false, false, false
+    elseif key == "right" and state == states.running then
+        left, right, up, down = false, true, false, false
+    elseif key == "up" and state == states.running then
+        left, right, up, down = false, false, true, false
+    elseif key == "down" and state == states.running then
+        left, right, up, down = false, false, false, true
+    elseif key == "space" and state == states.gameover then
+        start()
+    end
+end
+
+function start()
+    state = states.running
+    --directions from the keypressed event default is up
     left, right, up, down = false, false, true, false
-  elseif key == "down" and state==GameStates.running then
-    left, right, up, down = false, false, false, true
-  elseif key == "space" and state == GameStates.game_over then
-    game_start()
-  end
- 
+    dirX = 0
+    dirY = -1
+    --position for snake's head in center
+    head = { width / 2, (height - 1) / 2 }
+    --table for the snake's tail
+    tail = {}
+    --set speed and timer
+    speed = 0.25
+    timer = 0
+    --table for the apple
+    apple = {}
+    apple = getfreepos()
+end
+
+function getfreepos()
+    --random number total game area minus the snake
+    local randompos = math.random(width * height - (#tail + 1))
+    --free flag and counter
+    local free = true
+    local cnt = 0
+    --iterate in game area, when snake free variable set to false
+    for i = 0, width - 1 do
+        for j = 0, height - 1 do
+            --the snake's head
+            if head[1] == i and head[2] == j then
+                free = false
+            end
+            --the snake's tail
+            for _, v in ipairs(tail) do
+                if v[1] == i and v[2] == j then
+                    free = false
+                end
+            end
+            if free then
+                cnt = cnt + 1
+                if cnt == randompos then
+                    --if counter catch the random number return with the free position
+                    return { i, j }
+                end
+            else
+                free = true
+            end
+        end
+    end
 end
