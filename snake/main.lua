b4181e5af4b7d0ccc78296de68b0d2ea757f9829
k4gnes <kardosagnes@gmail.com>
2024-10-03 18:49:12 +0200

    compatible with loveputer

--- snake/main.lua
+++ snake/main.lua
@@ -1,7 +1,24 @@
+--size for draw
+unit = 10
+--game area dimensions
+width = 102
+height = 60
+love.window.setTitle("snake")
+
+normallove = false
+status = "running"
+
 function love.load()
-    -- initialize random number generator
-    math.randomseed(os.time())
     --start/restart game
+    if (G == nil) then
+        G = love.graphics
+        love.window.setMode(1024, 600)
+        midx, midy = G.getDimensions()
+        normallove = true
+    else
+        normallove = false
+    end
+
     start()
 end
 
@@ -34,68 +51,80 @@ direction = {
 
 function runningDraw()
     --draw the snake rounded rectangle
-    love.graphics.setColor(0, 0.8, 0)
+    G.setColor(0, 0.8, 0)
     for _, snake_part in pairs(snake) do
-        love.graphics.rectangle("fill", snake_part.x * unit, snake_part.y * unit, unit, unit, 5, 5);
+        G.rectangle("fill", snake_part.x * unit, snake_part.y * unit, unit, unit, 5, 5);
     end
 
     --draw the apple rounded rectangle just like snake
-    love.graphics.setColor(0.8, 0, 0)
-    love.graphics.rectangle("fill", apple.x * unit, apple.y * unit, unit, unit, 5, 5)
+    G.setColor(0.8, 0, 0)
+    G.rectangle("fill", apple.x * unit, apple.y * unit, unit, unit, 5, 5)
+
+    if status == "gameover" then
+        G.setColor(1, 1, 1)
+        G.setNewFont(20)
+        gameovertext = "GAME OVER\nPress [SPACE] for a new game\nor\n[ESCAPE] for leave"
+        G.printf(gameovertext, midx / 2 - 150, midy / 2, 300, "center")
+    end
+
 end
 
 function gameoverDraw()
     runningDraw()
     --draw the game over screen
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.setNewFont(20)
-    love.graphics.printf("GAME OVER\nPress [SPACE] for a new game", 0, height * 10 / 2 - 50, width * 10, "center")
+    -- G.setColor(1, 1, 1)
+    --  G.setNewFont(20)
+    -- G.printf("GAME OVER\nPress [SPACE] for a new game", 0, width*10/2,height*10/2, "center")
 end
 
 function runningUpdate(dt)
-    --time limit for speed of snake
-    timer = timer + dt
-    if timer < speed then
-        return
-    end
+    if status == "running" then
+        --time limit for speed of snake
+        timer = timer + dt
+        if timer < speed then
+            return
+        end
 
-    local last_head_position = { x = snake[1].x, y = snake[1].y }
-    move()
+        local last_head_position = { x = snake[1].x, y = snake[1].y }
+        move()
 
-    --check food
-    if snake[1].x == apple.x and snake[1].y == apple.y then
-        --add food and increase the snake
-        apple = get_free_position()
-        table.insert(snake, {  })
-    end
+        --check food
+        if snake[1].x == apple.x and snake[1].y == apple.y then
+            --add food and increase the snake
+            apple = get_free_position()
+            table.insert(snake, {  })
+        end
 
-    --set snake snake parts
-    --from snake end to second
-    for snake_pos = #snake, 3, -1 do
-        snake[snake_pos] = snake[snake_pos - 1]
-    end
+        --set snake snake parts
+        --from snake end to second
+        for snake_pos = #snake, 3, -1 do
+            snake[snake_pos] = snake[snake_pos - 1]
+        end
 
-    if #snake > 1 then
-        snake[2] = last_head_position
-    end
+        if #snake > 1 then
+            snake[2] = last_head_position
+        end
 
-    --check if game is over
-    --because snake's head is out of the screen
-    if snake[1].x < 0 or snake[1].y < 0 or snake[1].x > width - 1 or snake[1].y > height - 1 then
-        --game is over
-        gameover()
-    end
+        --check if game is over
+        --because snake's head is out of the screen
+        if snake[1].x < 0 or snake[1].y < 0 or snake[1].x > width - 1 or snake[1].y > height - 1 then
+            --game is over
+            gameover()
+            status = "gameover"
+        end
 
-    --because snake's head is in the snake
-    for _, snake_part in pairs(snake) do
-        if _ > 1 then
-            if snake[1].x == snake_part.x and snake[1].y == snake_part.y then
-                --game is over
-                gameover()
+        --because snake's head is in the snake
+        for _, snake_part in pairs(snake) do
+            if _ > 1 then
+                if snake[1].x == snake_part.x and snake[1].y == snake_part.y then
+                    --game is over
+                    gameover()
+                    status = "gameover"
+                end
             end
         end
+        timer = 0
     end
-    timer = 0
 end
 
 function gameoverUpdate(dt)
@@ -124,6 +153,12 @@ heading = {
         if direction.up ~= move then
             move = direction.down
         end
+    end,
+    space = function()
+        start()
+    end,
+    escape = function()
+        love.event.quit()
     end
 }
 
@@ -137,16 +172,24 @@ function gameoverKeypressed(key)
     if key == "space" then
         start()
     end
+    if key == "escape" then
+        love.event.quit()
+    end
 end
 
 function start()
+    math.randomseed(os.time())
+    if (G ~= nil) then
+        midx, midy = G.getDimensions()
+    end
     running()
+    status = "running"
     move = direction.up
     --position for snake's head in center
     --table for the snake
     snake = { {
                   x = width / 2,
-                  y = (height - 1) / 2
+                  y = (height) / 2
               } }
     --set speed and timer
     speed = 0.25
@@ -182,4 +225,8 @@ function get_free_position()
             end
         end
     end
+end
+
+if (normallove == false) then
+    start()
 end
